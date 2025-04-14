518c38a5936f685fc3c14bd10267fbf793f30151
Walace <walpxt2006@gmail.com>
2025-04-14 19:24:44 -0300

    Versão Walace rodando

--- MuseuVirtual/routes/web.php
+++ MuseuVirtual/routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\BannerController;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/walace', [BannerController::class, 'index']);
