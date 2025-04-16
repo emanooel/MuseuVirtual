0dd179143ba1e563b5f986115c9a9a223086f81e
Dxrkzz <marlonvicentinisilva@gmail.com>
2025-04-16 15:09:49 -0300

    Fazendo a cópia do projeto para fazer o pessoal

---
+++ MuseuVirtual/app/Http/Controllers/FotosController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Fotos;
+use Illuminate\Http\Request;
+
+class FotosController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Fotos $fotos)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Fotos $fotos)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Fotos $fotos)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Fotos $fotos)
+    {
+        //
+    }
+}
