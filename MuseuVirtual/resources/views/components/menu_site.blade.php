253048735bc74e8203ea8bad2af64133e43c2818
Livia <vieiraleallivia@gmail.com>
2025-05-19 10:27:07 -0300

    feat:Componente Menu

---
+++ MuseuVirtual/resources/views/components/menu_site.blade.php
@@ -0,0 +1,13 @@
+<header>
+    <br>
+    <nav class="rounded-full bg-[#F1EEDD] ml-80 mr-80 font-bold">
+        <ul class="flex justify-center font-[arial] pt-3 pb-3">
+            <li><a href="#" class="p-5 pr-40 text-[#565851]">Museu Virtual ES</a></li>
+            <li><a href="#" class="p-5 text-[#565851]">JAZIDAS</a></li>
+            <li><a href="#" class="p-5 text-[#565851]">ROCHAS</a></li>
+            <li><a href="#" class="p-5 pr-40 text-[#565851]">MINERAIS</a></li>
+            <li><a href="#" class="p-2 pl-5 pr-5 rounded-full bg-[#ACB18E] mr-5 text-[#F1EEDD]">CADASTRAR</a></li>
+            <li><a href="#" class="p-2 pl-5 pr-5 rounded-full bg-[#ACB18E] text-[#F1EEDD]">LOGIN</a></li>
+        </ul>
+    </nav>
+</header>
