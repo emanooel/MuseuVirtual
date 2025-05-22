ec6b434f7558f181fdf0e5e20b287761e2b9e83b
Livia <vieiraleallivia@gmail.com>
2025-05-22 09:57:55 -0300

    :sparkles: feat:pagina inicial

--- MuseuVirtual/resources/views/components/rodape_site.blade.php
+++ MuseuVirtual/resources/views/components/rodape_site.blade.php
@@ -1,17 +1,32 @@
 <div class="flex justify-center mt-10">
-<footer class="grid grid-flow-col grid-rows-2 gap-4 pt-40 ">
-    <form class="row-span-3 p-4 rounded-xl bg-[#F1EEDD] text-[#565851] font-[arial] " action="" action="get">
-        <h3>Entre com sua conta no Museu Virtual</h3>
-        <input type="text" id="login" name="login" placeholder="Login">
-        <br>
-        <input type="text" id="Senha" name="Senha" placeholder="Senha">
-        <br>
-        <input type="button" value="Entrar" class="row-span-3 pr-5 pl-5 rounded-xl bg-[#ACB18E]">
-        <br>
-        <a href="">Ainda não se cadastrou? Cadastre-se <strong>AQUI</strong></a>
-    </form>
-    <img class="imagem_final col-span-2" src="/assets/img/pedraFinal.png" alt="">
-    <img class="imagem_final col-span-2 row-span-2 p-1 pl-9 pr-9 rounded-xl bg-[#F1EEDD]"
-        src="/assets/img/logoifes 1.png" alt="Logo do IFES">
-</footer>
-<div>
+    <footer class="grid grid-cols-2 gap-4 pt-40 pb-10">
+        <form class="row-span-3 p-8 rounded-xl bg-[#F1EEDD] text-[#565851] font-[arial] shadow-xl text-[20px]" action=""
+            action="get">
+            <h2>Entre com sua conta no Museu Virtual</h2>
+
+            <div class="mb-4 mt-4">
+                <input type="text" id="login" name="login"
+                    class="w-full px-3 py-2 border-b border-0 bg-transparent text-[#565851]" placeholder="Login">
+            </div>
+
+            <div class="mb-6">
+                <input type="password" id="senha" name="senha"
+                    class="w-full px-3 py-2 border-b border-0 bg-transparent text-[#565851]" placeholder="Senha">
+            </div>
+
+            <button type="submit"
+                class="bg-[#ACB18E] hover:bg-[#A39D8C] text-[#565851] font-normal py-2 px-6 rounded-full w-full mb-4 focus:outline-none focus:shadow-outline">
+                Entrar
+            </button>
+
+            <p class="text-[18px] text-[#565851] text-center">
+                Ainda não se cadastrou? <a href="" class="text-[#565851] hover:underline font-medium">Cadastre-se
+                    <strong>AQUI</strong></a>
+            </p>
+        </form>
+        <img  src="/assets/img/pedraFinal.png" alt="">
+
+        <img class="p-1 pl-9 pr-9 rounded-xl bg-[#F1EEDD] p-4" src="/assets/img/logoifes 1.png"
+            alt="Logo do IFES">
+    </footer>
+    <div>
--- MuseuVirtual/resources/views/home.blade.php
+++ MuseuVirtual/resources/views/home.blade.php
@@ -11,21 +11,20 @@
     @endif
 </head>
 
-<body class="bg-[#363C27]">
-
+<body class="bg-gradient-to-b from-[#ACB18E] from-0.5% via-[#73785C] via-5% to-[#363C27] to-20% ">
     <x-menu_site></x-menu_site>
-    <h1 class="font-[arial] pt-40 font-bold text-[70px] text-center text-[#F1EEDD]">Museu Virtual</h1>
-    <h2 class="text-center text-[15px] font-[arial] text-[#F1EEDD]">Bem-vindo ao Museu Virtual das Rochas, um <br>espaço
+    <h1 class="font-[arial] pt-40 font-bold text-[80px] text-center text-[#F1EEDD]">Museu Virtual</h1>
+    <h2 class="text-center text-[20px] font-[arial] text-[#F1EEDD]">Bem-vindo ao Museu Virtual das Rochas, um <br>espaço
         interativo e educativo dedicado à incrível<br> diversidade geológica da nossa região. </h2>
     <br>
     <p class="text-center"><a href="#" class="p-1 pl-9 pr-9 rounded-full bg-[#F1EEDD] text-[#565851]">Login</a></p>
-    <figure class="w-100 mt-10">
+    <figure class="w-100 mt-20">
         <img class="w-full" src="/assets/img/image 1.png" alt="Imagem de uma jazida ">
     </figure>
 
     <div class="flex justify-center mr-60 pt-40">
         <h2
-            class="font-[arial] font-bold text-[70px] bg-gradient-to-b from-[#F1EEDD] to-[#363C27] bg-clip-text text-transparent">
+            class="font-[arial] font-bold text-[80px] bg-gradient-to-b from-[#F1EEDD] to-[#363C27] bg-clip-text text-transparent">
             Explore</h2>
     </div>
     <div class="flex justify-center ml-60">
