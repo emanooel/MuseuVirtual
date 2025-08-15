a1ca418a0a500fcc4e6bdf98a3df9b66a11f74f8
Caio-456 <caiomottabarcelos13@gmail.com>
2025-08-15 15:55:56 -0300

    :bug:fix:rotas de rocha

--- MuseuVirtual/routes/web.php
+++ MuseuVirtual/routes/web.php
@@ -43,7 +43,7 @@ Route::middleware('auth')->group(function () {
 
 Route::get('/dashboard/rocha', [RochaController::class, 'index'])->name('rochas.index');
 
-Route::resource('rochas', RochaController::class)->names('rochas');
+Route::resource('rocha', RochaController::class)->names('Rocha');
 
 Route::resource('timeline', RochaController::class)->names('Timeline');
 
