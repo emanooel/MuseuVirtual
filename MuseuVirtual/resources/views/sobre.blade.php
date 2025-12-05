dde8639426ec9b2ac1dd15e346517e1457a07388
liviascaramussa <pezzinscaramussalivia@gmail.com>
2025-12-05 11:05:19 -0300

    Personalizamos a página sobre

--- MuseuVirtual/resources/views/sobre.blade.php
+++ MuseuVirtual/resources/views/sobre.blade.php
@@ -1,5 +1,83 @@
 <x-layouts.BaseLayout>
+        @vite(['resources/css/sobre.css'])
     <x-slot name="title">Sobre</x-slot>
-    <p>Implemente aqui a pagina sobre o projeto</p>
-    <p>o que é o museu virtual mineral ES, e a equipe responsavel</p>
+    <div class="hero-container">
+        <div class="hero-content">
+            <h1 class="main-title">Sobre o projeto</h1>
+            <p class="subtitle">
+                 Plataforma digital interativa para funcionar como um repositório virtual da riqueza
+                  mineralógica do Espírito Santo, com foco especial na região sul do estado.
+            </p>
+        </div>
+    </div>
+    <div class="explore-section">
+        <h2 class="explore-title">Nossa Equipe</h2>
+    </div>
+
+<section>
+    <div>
+        <div class="membros-container">
+        <div class="membro">
+            <div class="foto-borda">
+                <img src="livia.lisa.jpg" alt="Foto de Livia Lisa">
+            </div>
+            <p class="nome">Lívia Scaramussa</p>
+            <p class="cargo">Estudante</p>
+        </div>
+
+        <div class="membro">
+            <div class="foto-borda">
+                <img src="livia.cachos.jpg" alt="Foto de Livia Cachos">
+            </div>
+            <p class="nome">Lívia Leal</p>
+            <p class="cargo">Estudante</p>
+        </div>
+
+        <div class="membro">
+            <div class="foto-borda">
+                <img src="Gabriel.jpg" alt="Foto de Gabriel Melo">
+            </div>
+            <p class="nome">Gabriel Melo</p>
+            <p class="cargo">Estudante</p>
+        </div>
+
+        <div class="membro">
+            <div class="foto-borda">
+                <img src="Marlon.jpg" alt="Foto de Marlon">
+            </div>
+            <p class="nome">Marlon Vicentini</p>
+            <p class="cargo">Estudante</p>
+        </div>
+    </div>
+    <div class="membros-container-2">
+        <div class="membro">
+            <div class="foto-borda">
+                <img src="Caio.jpg" alt="Foto de Caio">
+            </div>
+            <p class="nome">Caio Motta</p>
+            <p class="cargo">Estudante</p>
+        </div>
+        <div class="membro">
+            <div class="foto-borda">
+                <img src="Wallace.jpg" alt="Foto de Wallace">
+            </div>
+            <p class="nome">Walace Louzada</p>
+            <p class="cargo">Estudante</p>
+        </div>
+        <div class="membro">
+            <div class="foto-borda">
+                <img src="Emanoel.jpg" alt="Foto de Emanoel">
+            </div>
+            <p class="nome">Emanoel Martins</p>
+            <p class="cargo">Tutor</p>
+        </div>
+        <div class="membro">
+            <div class="foto-borda">
+                <img src="RafaelGuimarães.jpg" alt="Foto de Rafael Guimarães">
+            </div>
+            <p class="nome"> Rafael Guimarães</p>
+            <p class="cargo">Coordenador</p>
+        </div>
+    </div>
+</section>
 </x-layouts.BaseLayout>
